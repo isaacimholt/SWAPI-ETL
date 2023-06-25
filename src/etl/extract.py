fac61df86e0da478fd7598d639979c8fbd1b7343
Isaac Imholt <isaacimholt@gmail.com>
2023-06-25 23:24:52 +0200

    use path.join() instead of string concat

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -19,8 +19,8 @@ async def extract(settings: Settings) -> AsyncIterator[Person]:
     connector = aiohttp.TCPConnector(limit=settings.max_simultaneous_requests)
     client_session = aiohttp.ClientSession(connector=connector, raise_for_status=True)
     async with client_session as client:
-        # todo: use path join
-        person_page = await get_person_page(client, settings.api_url_base + "people")
+
+        person_page = await get_person_page(client, path.join(settings.api_url_base, "people"))
         for person in person_page.results:
             yield person
 
@@ -28,9 +28,8 @@ async def extract(settings: Settings) -> AsyncIterator[Person]:
         if num_pages <= 1:
             return
 
-        # todo: use path join
         # todo: use url query constructor
-        urls = [settings.api_url_base + f"people/?page={i}" for i in range(2, num_pages + 1)]
+        urls = [path.join(settings.api_url_base, f"people/?page={i}") for i in range(2, num_pages + 1)]
         tasks = [get_person_page(client, url) for url in urls]
         tasks = tqdm(asyncio.as_completed(tasks), total=num_pages - 1)
         for task in tasks:
