327c3cdb599c62159f8b22e753b588de5da14dc0
Isaac Imholt <isaacimholt@gmail.com>
2023-06-25 21:12:07 +0200

    added transform for filtering & sorting people

---
+++ src/etl/transform.py
@@ -0,0 +1,50 @@
+import heapq
+from operator import attrgetter
+from typing import AsyncIterator
+
+from pandas import DataFrame
+
+from models import Person, PersonComparator
+from settings import Settings
+
+
+async def transform(people: AsyncIterator[Person], settings: Settings):
+    people = await filter_and_sort(people=people, settings=settings)
+    return people
+
+
+async def filter_and_sort(people: AsyncIterator[Person], settings: Settings) -> list[Person]:
+    # 1. Handle "large" datasets with heap queue
+    # 2. Ignore people that do not have a numeric height
+    # 3. Configurable number of max people to consider
+    # this function is overkill for the current data source (max 100 results from api)
+    # but wanted to demonstrate handling much larger data streams in memory-efficient way
+
+    # had some difficulty here, wanted to use a heapq.nlargest solution to handle "large" streaming data
+    # but heapq.nlargest handles only iter protocol and not async aiter, so had to do n-largest filter "by hand"
+    # most_films = heapq.nlargest(settings.max_person_filter, people, lambda p: len(p.films) and p.height is not None)
+
+    most_films_heap: list[PersonComparator] = []
+    async for person in people:
+
+        # skip non-numeric heights
+        if person.height is None:
+            continue
+
+        # create a comparison wrapper with person & inverse key because we have a min heap
+        person_with_inverse_key = PersonComparator(key=-len(person.films), person=person)
+
+        # push heap & handle max number of items
+        if len(most_films_heap) < settings.max_person_filter:
+            heapq.heappush(most_films_heap, person_with_inverse_key)
+        else:
+            heapq.heappushpop(most_films_heap, person_with_inverse_key)
+
+    # unwrap PersonComparator
+    most_films: list[Person] = [comparator.person for comparator in most_films_heap]
+
+    return sorted(most_films, key=attrgetter("height"), reverse=True)
+
+
+def to_df(people: list[Person]) -> DataFrame:
+    pass
--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import asyncio
 import logging
 
 from etl.extract import extract
+from etl.transform import transform
 from settings import Settings
 
 logger = logging.getLogger(__name__)
 
 
 async def etl(settings: Settings):
-    async for person in extract(settings=settings):
-        print(person)
+    people = extract(settings=settings)
+    people = await transform(people=people, settings=settings)
+    print(people)
 
 
 if __name__ == "__main__":
